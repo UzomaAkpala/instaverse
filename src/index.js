8e7dd8ada9537d05eee8c5fb98f8a01977ea9ef7
UzomaAkpala <francisakpala@yahoo.com>
2023-10-23 14:07:48 +0100

    Building Reducers in React JS

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import reducers from "./reducers";
 import App from "./App";
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
