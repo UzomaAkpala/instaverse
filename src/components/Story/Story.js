18759b8d50fa59abbca9241f4b82484eb7b454ff
UzomaAkpala <francisakpala@yahoo.com>
2023-10-24 10:02:50 +0100

    server side

--- src/components/Story/Story.js
+++ src/components/Story/Story.js
@@ -1,7 +1,69 @@
-//import styles from "./styles";
+import React, { useState } from "react";
+import { Card, Tooltip, Typography, Image } from "antd";
+import { EditOutlined, DeleteTwoTone, HeartTwoTone } from "@ant-design/icons";
+import moment from "moment";
 
-function Story() {
-  return <h1>Story</h1>;
+import styles from "./styles";
+
+const { Meta } = Card;
+const { Link, Paragraph, Text } = Typography;
+
+function Story({ story }) {
+  const [expand, setExpand] = useState(true);
+  return (
+    <Card
+      style={styles.card}
+      cover={
+        <Image
+          src={Story.image}
+          actions={[
+            <div style={styles.actions}>
+              <Tooltip
+                placement="top"
+                title="Like"
+                color="magenta"
+                onClick={() => {}}
+              >
+                <HeartTwoTone twoToneColor="magenta" />
+                &nbsp; {story.likes} &nbsp;
+              </Tooltip>
+            </div>,
+
+            <Tooltip placement="top" title="Edit">
+              <EditOutlined onClick={() => {}} />
+            </Tooltip>,
+
+            <Tooltip placement="top" title="Delete" color="red">
+              <DeleteTwoTone twoToneColor="red" onClick={() => {}} />
+            </Tooltip>,
+          ]}
+        />
+      }
+    >
+      <Meta title={story.username} />
+      <Paragraph
+        style={{ margin: 0 }}
+        ellipsis={{
+          row: 2,
+          expandable: true,
+          symbol: "more",
+          onExpand: () => {
+            setExpand(true);
+          },
+          onEllipsis: () => {
+            setExpand(false);
+          },
+        }}
+      >
+        {story.caption}
+      </Paragraph>
+      {expand ? (
+        <Link href="#">{story.tags.split(" ").map((tag) => `#${tag} `)}</Link>
+      ) : null}
+      <br />
+      <Text type="secondary">{moment(story.postDate).fromNow()}</Text>
+    </Card>
+  );
 }
 
 export default Story;
